b8fe7d5704b8f1973f16d1c1c4c45ece96bbd6d1
David Lebel <lebel@lebel.org>
2014-10-11 12:02:31 -0400

    since cron doesn't seem to have the variables defined in the run stage, we'll make sure the tarsnap.cron has it "hardcoded".

--- startup.sh
+++ startup.sh
@@ -10,3 +10,9 @@ if [ -e /config/authorized_keys ]; then
   cp /config/authorized_keys /root/.ssh/authorized_keys
   chmod 600 /root/.ssh/authorized_keys
 fi
+
+# make sure MAILTO is respected
+
+if [ -e /helpers/tarsnap.cron ]; then
+  sed -i -e "s;\$MAILTO;$MAILTO;g" /helpers/tarsnap.cron
+fi
